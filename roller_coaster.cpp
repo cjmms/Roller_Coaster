3f6b80e1d429a975bded707791a6f360ea0d5375
Xingyu Wang <brandon.xyw@gmail.com>
2019-10-17 14:23:23 -0400

    update

--- roller_coaster.cpp
+++ roller_coaster.cpp
@@ -13,51 +13,47 @@ const float BOX_SIZE = 7.0f;
 void draw() {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-	// glMatrixMode(GL_MODELVIEW);
-	// glLoadIdentity();
+	glLoadIdentity();
+	gluLookAt(	0.0, 0.0, 5.0, 
+				0.0, 0.0, 0.0, 
+				0.0, 1.0, 0.0);
 	
-	GLfloat ambientLight[] = {0.3f, 0.3f, 0.3f, 1.0f};
-	glLightModelfv(GL_LIGHT_MODEL_AMBIENT, ambientLight);
-	
-	GLfloat lightColor[] = {0.7f, 0.7f, 0.7f, 1.0f};
-	GLfloat lightPos[] = {-2 * BOX_SIZE, BOX_SIZE, 4 * BOX_SIZE, 1.0f};
-	glLightfv(GL_LIGHT0, GL_DIFFUSE, lightColor);
-	glLightfv(GL_LIGHT0, GL_POSITION, lightPos);
 
-	glutSolidTeapot(0.5);  // draw the teapot
+	glutSolidTeapot(1);  // draw the teapot
 
 	glutSwapBuffers();
 }
 
-void init() {
-	glViewport(0, 0, 480, 480);  // define a viewport and display in this viewport
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	gluPerspective(60.0, 1, 1.0, 20.0);  // specify a viewing frustum
-	glMatrixMode(GL_MODELVIEW);
-	glLoadIdentity();
-	gluLookAt(0.0, 0.0, 5.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);  // define a viewing matrix
-}
 
 void init_lighting() {
 	glEnable(GL_DEPTH_TEST);
-	glEnable(GL_LIGHTING);
-	glEnable(GL_NORMALIZE);
-	glEnable(GL_COLOR_MATERIAL);
-	glEnable(GL_LIGHT0);
+	// glEnable(GL_LIGHTING);
+	// glEnable(GL_NORMALIZE);
+	// glEnable(GL_COLOR_MATERIAL);
+	// glEnable(GL_LIGHT0);
+}
+
+void changeSize(int w, int h) {
+	float ratio = (float)w/ (float)h;
+
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glViewport(0, 0, w, h);
+	gluPerspective(60.0, ratio, 1.0, 100.0);  // specify a viewing frustum
+	glMatrixMode(GL_MODELVIEW);
 }
 
 int main(int argc, char *argv[])
 {
 	glutInit(&argc, argv);  // initialize the library
 	glutInitDisplayMode(GLUT_RGBA | GLUT_DEPTH | GLUT_DOUBLE);
-	glutInitWindowSize(480, 480);  // define size of the window
-	glutInitWindowPosition(0, 0);  // define position of the window
+	glutInitWindowSize(600, 480);  // define size of the window
+	glutInitWindowPosition(100, 100);  // define position of the window
 	glutCreateWindow("Hello teapot");
 
-	init();
 	init_lighting();
 
+	glutReshapeFunc(changeSize);
 	glutDisplayFunc(draw);  // display callback
 	glutMainLoop();  // enter event processing loop
 
