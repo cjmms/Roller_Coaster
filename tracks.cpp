94f296d7425b8e77914eb51d501f6e666c58eb1e
Xingyu Wang <brandon.xyw@gmail.com>
2019-10-20 16:31:23 -0400

    add another level section

--- tracks.cpp
+++ tracks.cpp
@@ -188,4 +188,7 @@ void drawTracks() {
     glTranslated(0, sin(30 * M_PI / 180.0) * 50 * segLength, cos(30 * M_PI / 180.0) * 60 * -segLength   + 9);
     downturn(segLength / 2, 0, 0, 30);
 
+    // --------
+    glTranslated(0, sin(30 * M_PI / 180.0) * 10 * segLength, cos(30 * M_PI / 180.0) * 10 * -segLength + 1.8);
+    levelSection(100, 0);
 }
